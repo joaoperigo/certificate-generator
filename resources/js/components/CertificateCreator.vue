2dcef4c9d11f6b869fabdc1e86053a017b9f7514
João Cabral Périgo <joao.perigo@devio.com.br>
2024-09-16 02:05:14 -0300

    some features

--- resources/js/components/CertificateCreator.vue
+++ resources/js/components/CertificateCreator.vue
@@ -36,7 +36,7 @@
         
         <button 
           @click="saveCertificate" 
-          class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
         >
           Save Certificate
         </button>
