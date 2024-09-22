8b8f2480b589f55271e5f70820dc71265994864c
João Cabral Périgo <joao.perigo@devio.com.br>
2024-09-22 05:39:30 -0300

    Bkp for canvas for fruther analisys

--- resources/js/components/CanvasEditor.vue
+++ resources/js/components/CanvasEditor.vue
@@ -1,5 +1,8 @@
+<!-- CanvasEditor.vue -->
 <template>
-  <canvas ref="canvas"></canvas>
+  <div class="canvas-editor overflow-y p-4">
+    <canvas class="w-full h-auto shadow-xl bg-stone-100" ref="canvas"></canvas>
+  </div>
 </template>
 
 <script>
@@ -14,58 +17,96 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      ctx: null,
+      canvasWidth: 303.02 * 3.779528, // mm to px
+      canvasHeight: 215.98 * 3.779528 // mm to px
+    }
+  },
+  mounted() {
+    this.initCanvas()
+  },
   watch: {
-    currentPage() {
-      this.renderPage()
+    currentPage: {
+      handler() {
+        this.redrawCanvas()
+      },
+      immediate: true
     },
     pages: {
       deep: true,
       handler() {
-        this.renderPage()
+        this.redrawCanvas()
       }
     }
   },
-  mounted() {
-    this.renderPage()
-  },
   methods: {
-    renderPage() {
+    initCanvas() {
       const canvas = this.$refs.canvas
-      const ctx = canvas.getContext('2d')
+      canvas.width = this.canvasWidth
+      canvas.height = this.canvasHeight
+      this.ctx = canvas.getContext('2d')
+      this.redrawCanvas()
+    },
+    redrawCanvas() {
+      if (!this.ctx) return
+      this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
+      const currentPageData = this.pages[this.currentPage]
       
-      // Verificar se há páginas antes de tentar renderizar
-      if (this.pages.length === 0 || !this.pages[this.currentPage]) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-        ctx.fillStyle = 'gray'
-        ctx.fillText('No page data available', 10, 50)
-        return
+      if (currentPageData && currentPageData.backgroundImage) {
+        this.drawBackgroundImage(currentPageData.backgroundImage)
       }
-
-      const page = this.pages[this.currentPage]
-
-      // Limpar o canvas
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-      // Renderizar o background
-      if (page.backgroundImage) {
-        const img = new Image()
-        img.onload = () => {
-          ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
-          this.renderObjects(ctx, page.objects)
-        }
-        img.src = page.backgroundImage
-      } else {
-        this.renderObjects(ctx, page.objects)
+      
+      if (currentPageData && currentPageData.objects) {
+        this.drawObjects(currentPageData.objects)
       }
     },
-    renderObjects(ctx, objects) {
+    drawBackgroundImage(imageUrl) {
+      const img = new Image()
+      img.onload = () => {
+        this.ctx.drawImage(img, 0, 0, this.canvasWidth, this.canvasHeight)
+        // Redesenhar os objetos após carregar a imagem de fundo
+        this.drawObjects(this.pages[this.currentPage].objects)
+      }
+      img.src = imageUrl
+    },
+    drawObjects(objects) {
       if (!objects) return
-      objects.forEach(obj => {
-        ctx.font = `${obj.fontSize}px ${obj.fontFamily}`
-        ctx.fillStyle = obj.fontColor
-        ctx.textAlign = obj.textAlign || 'left'
-        ctx.fillText(obj.text, obj.xPos, obj.yPos)
-      })
+      objects.forEach(obj => this.drawObject(obj))
+    },
+    drawObject(obj) {
+      this.ctx.font = `${obj.fontSize}px ${obj.fontFamily}`
+      this.ctx.fillStyle = obj.fontColor
+      this.ctx.textAlign = obj.textAlign || 'left'
+
+      const words = obj.text.split(' ')
+      let line = ''
+      let y = obj.yPos
+
+      for (let word of words) {
+        const testLine = line + word + ' '
+        const metrics = this.ctx.measureText(testLine)
+        const testWidth = metrics.width
+
+        if (testWidth > obj.boxWidth && line !== '' && obj.boxWidth > 0) {
+          this.drawAlignedText(line, obj.xPos, y, obj)
+          line = word + ' '
+          y += obj.fontSize * 1.2
+        } else {
+          line = testLine
+        }
+      }
+      this.drawAlignedText(line, obj.xPos, y, obj)
+    },
+    drawAlignedText(text, x, y, obj) {
+      let adjustedX = x
+      if (obj.textAlign === 'center') {
+        adjustedX = x + obj.boxWidth / 2
+      } else if (obj.textAlign === 'right') {
+        adjustedX = x + obj.boxWidth
+      }
+      this.ctx.fillText(text, adjustedX, y)
     }
   }
 }
