28eb8f4cbed1686e342b2791ed66ed5dfbd1563d
João Cabral Périgo <joao.perigo@devio.com.br>
2025-02-08 01:43:30 -0300

    Upgrading to Laravel 11

--- routes/web.php
+++ routes/web.php
@@ -2,58 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\CertificateController;
-use App\Http\Controllers\ImageController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-
-
-
-// Route::resource('images', ImageController::class);
-// Route::post('/images/upload', [ImageController::class, 'upload'])->name('images.upload');
-// Route::post('/images/upload', [ImageController::class, 'upload'])->name('images.upload');
-// Route::get('/images/list', [ImageController::class, 'indexJson']);
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-
-    Route::resource('certificates', CertificateController::class);
-
-    Route::put('/certificates/{certificate}/update-texts', [CertificateController::class, 'updateTexts']);
-
-    Route::get('certificate-images/{certificateImage}', [CertificateImageController::class, 'show'])
-        ->name('certificate.images.show');
-
-    Route::post('/api/images', [ImageController::class, 'store']);
-    Route::get('/images/{image}', [ImageController::class, 'show'])->name('images.show');
-    
-    Route::delete('/api/images/{image}', [ImageController::class, 'destroy']);
-
-});
-
-
-
-// Route::get('/test', function () {
-//     return '{"file_path": "12354123"}';
-// });
