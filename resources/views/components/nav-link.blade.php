7b1bb42979b072479e7d68f7b49900479b7008a7
João Cabral Périgo <joao.perigo@devio.com.br>
2025-02-18 18:39:35 -0300

    Added link of code search on nav/ ++style

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-? 'border border-b-4 border-stone-600 hover:bg-purple-300 text-stone-900 bg-purple-400 font-bold py-3 ps-2 pe-4 rounded-xl mt-3 flex justify-center gap-2 items-center h-[44px]'
-: 'border border-b-4 border-stone-600 hover:bg-purple-300 text-stone-900 bg-purple-400 font-bold px-3 mt-3 rounded-xl flex justify-center gap-2 items-center h-[44px]';
+? 'border border-b-4 border-purple-900 hover:bg-purple-300 text-stone-900 bg-purple-400 font-bold py-3 ps-2 pe-4 rounded-xl mt-3 flex flex-col justify-end gap-2 items-center w-14 h-14 text-xs pb-1'
+: 'border border-b-4 border-purple-700 hover:bg-purple-300 text-stone-900 bg-purple-400 font-bold px-3 mt-3 rounded-xl flex flex-col justify-end gap-2 items-center w-14 h-14 text-xs pb-1';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
