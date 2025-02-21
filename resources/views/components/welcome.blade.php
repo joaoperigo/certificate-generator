34a945ed888479134e352672e55cc6e6ea0796a4
João Cabral Périgo <joao.perigo@devio.com.br>
2025-02-21 01:47:03 -0300

    Fixed scrolling on others views than certificates

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -2,15 +2,20 @@
     <x-application-logo class="block h-12 w-auto" />
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900">
-        Welcome to your Jetstream application!
+        Códigos que podem ser usados
     </h1>
 
-    <p class="mt-6 text-gray-500 leading-relaxed">
-        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-        ecosystem to be a breath of fresh air. We hope you love it.
-    </p>
+    <ul class="mt-6 text-gray-500 leading-relaxed">
+        <li>[name]</li>
+        <li>[cpf]</li>
+        <li>[document]</li>
+        <li>[code]</li>
+        <li>[unit]</li>
+        <li>[course]</li>
+        <li>[quantity_hours]</li>
+        <li>[start_date]</li>
+        <li>[end_date]</li>
+    </ul>
 </div>
 
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
